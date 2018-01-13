473384749b1f098e9eeafe0d301dbc60927c381f
Richard Pham <rpham@localhost.localdomain>
2018-01-12 21:24:45 -0700

    Framework

---
+++ JAVA/a1.java
@@ -0,0 +1,10 @@
+class a1{
+  public static void main(String args[]){
+    //get hard constraint data
+    //get soft constraint data
+    //create a tree
+    //preform a depth first search
+    //itterate through the tree until fully explored
+    //return the assignment with the lowest pen. 
+  }
+}
