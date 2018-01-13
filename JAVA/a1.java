250060bdd71d265bf3cea53eba3720e133fb5653
Richard Pham <rpham@localhost.localdomain>
2018-01-12 21:45:14 -0700

    Basic node

--- JAVA/a1.java
+++ JAVA/a1.java
@@ -1,10 +1,26 @@
 class a1{
+  //the nodes of the binary tree
+  public class node {
+    public node children[];
+    public int mach;
+    public int task;
+    public int flag;
+    public int pen;
+
+    public void node(int m,int t,int f,int p){
+      this.mach = m;
+      this.task = t;
+      this.flag = f;
+      this.pen = p;
+    }
+
+  }
   public static void main(String args[]){
     //get hard constraint data
     //get soft constraint data
     //create a tree
     //preform a depth first search
     //itterate through the tree until fully explored
-    //return the assignment with the lowest pen. 
+    //return the assignment with the lowest pen.
   }
 }
