47a45dd8fa7a9b8792ab8cf4e0badce5833a2d09
sarahhatcher <sarah.hatchersmallma@ucalgary.ca>
2018-02-07 18:13:00 -0700

    Final Scheduler

--- JAVA/Scheduler.java
+++ JAVA/Scheduler.java
@@ -1,12 +1,6 @@
 /*
     Team: Splash Zone
-    Members:
-        -
-        -
-        -
-        -
-        -
-
+   
 	# Targeted for OpenJDK 1.8.0_151
 
 	-------------------------------------------------------
@@ -14,23 +8,6 @@
 
 		- Program will accept file to find the minimum solution to given problem.
 
-	Known Issues:
-		* TBA
-
-	Known Limits:
-		* TBA
-
-	Revision History:
-		Information on non-functioning version will be excluded.
-
-        v0.1: Completed early implementation of SplashParser
-        v0.2: SplashParser now can handle all ASCII cases except for the bracket.
-        v0.3: SplashParser now correctly handles penalty error, debug function 'systemStatePrinter' added. Private.
-        v0.4: SplashParser updated, detail on SplashParser
-        v0.5: SplashParser updated, detail on SplashParser.
-                Added implementation of SplashOutput
-                Added implementation of SplashSearch
-                Added implementation of Node
 */
 
 public class Scheduler
@@ -44,6 +21,7 @@ public class Scheduler
             System.out.println("java Scheduler (inputName) (outputName)");
             System.exit(0);
         }
+        outputFile = args[1];
         int[][][] pntArray = new int[8][8][2];
         try {
             SplashParser parseInstance = new SplashParser(args[0]);
@@ -52,10 +30,10 @@ public class Scheduler
             System.out.println("Unhandled General Error");
             SplashOutput.printError(1);
         }
-        outputFile = args[1];
+        
         
 
-        splashSearch searchInstance = new splashSearch(pntArray);
+        SplashSearch searchInstance = new SplashSearch(pntArray);
         searchInstance.treeConstruct();
     }
 }
